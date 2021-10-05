f81a311ae27c0d62d482d120db7e827e8c461799
clroot <abcdkh1209@gmail.com>
2021-10-06 00:51:09 +0900

    write main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import Responsive from "../components/common/Responsive";
-import {Helmet} from "react-helmet-async";
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+import Responsive from '../components/common/Responsive';
+import Button from '../components/common/Button';
+
+const StyledImage = styled.img`
+  width: 150px;
+  margin-top: 50px;
+`;
+
+const StyledText = styled.p`
+  span {
+    display: block;
+    font-weight: normal;
+  }
+
+  font-weight: bold;
+`;
 
 const Main = () => {
   return (
@@ -8,11 +24,23 @@ const Main = () => {
       <Helmet>
         <title>슈아가 언제 졸업할까?</title>
       </Helmet>
-      <Responsive style={{height: '50vh'}}>
-        <div style={{border: '1px solid grey', minHeight: '100%', minWidth: '100%'}}>BORDER_LAYOUT</div>
+      <Responsive>
+        <StyledImage src="/images/logo-clean.png" alt="ssuniverse-logo" />
+        <StyledText>
+          <span>숭실대의 숨겨진 재학생 슈아</span>
+          슈아가 언제 졸업할까?
+        </StyledText>
+        <img
+          src="/images/xbox.png"
+          alt="xbox-image"
+          style={{ width: '400px' }}
+        />
+        <Button fullWidth blue>
+          테스트 시작
+        </Button>
       </Responsive>
     </>
   );
-}
+};
 
 export default Main;
