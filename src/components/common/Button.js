1ac30f27f822962c2bf95ceb2886f42a50bfa729
clroot <abcdkh1209@gmail.com>
2021-10-06 00:50:13 +0900

    write palette & Button Component

---
+++ src/components/common/Button.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+
+const buttonStyle = css`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.25rem 1rem;
+  color: white;
+  outline: none;
+  cursor: pointer;
+
+  background: '${palette.gray[8]}';
+
+  &:hover {
+    background: ${palette.gray[6]};
+  }
+
+  &:disabled {
+    background: ${palette.gray[3]};
+    color: ${palette.gray[5]};
+    cursor: not-allowed;
+  }
+
+  ${(props) =>
+    props?.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+
+  ${(props) =>
+    props?.indigo &&
+    css`
+      background: ${palette.indigo[5]};
+
+      &:hover {
+        background: ${palette.indigo[4]};
+      }
+    `}
+
+  ${(props) =>
+    props?.blue &&
+    css`
+      background: ${palette.blue[5]};
+
+      &:hover {
+        background: ${palette.blue[4]};
+      }
+    `}
+`;
+
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledA = styled.a`
+  display: block;
+  text-align: center;
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  text-align: center;
+  ${buttonStyle}
+`;
+
+const Button = (props) => {
+  if (props.to) {
+    return (
+      <StyledLink
+        {...props}
+        indigo={props?.indigo ? 1 : 0}
+        blue={props?.blue ? 1 : 0}
+      />
+    );
+  } else if (props.href) {
+    return <StyledA {...props} />;
+  } else {
+    return <StyledButton {...props} />;
+  }
+};
+
+export default Button;
---
+++ src/lib/styles/palette.js
@@ -0,0 +1,40 @@
+const palette = {
+  gray: [
+    '#f8f9fa',
+    '#f1f3f5',
+    '#e9ecef',
+    '#dee2e6',
+    '#ced4da',
+    '#adb5bd',
+    '#868e96',
+    '#495057',
+    '#343a40',
+    '#212529',
+  ],
+  indigo: [
+    '#edf2ff',
+    '#dbe4ff',
+    '#bac8ff',
+    '#91a7ff',
+    '#748ffc',
+    '#5c7cfa',
+    '#4c6ef5',
+    '#4263eb',
+    '#3b5bdb',
+    '#364fc7',
+  ],
+  blue: [
+    '#e7f5ff',
+    '#d0ebff',
+    '#a5d8ff',
+    '#74c0fc',
+    '#4dabf7',
+    '#339af0',
+    '#228be6',
+    '#1c7ed6',
+    '#1971c2',
+    '#1864ab',
+  ],
+};
+
+export default palette;
