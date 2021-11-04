9a2a153123c92f3151e76906a5f07ac3b0ad1ad2
clroot <abcdkh1209@gmail.com>
2021-11-04 13:50:08 +0900

    update Logo image and component

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import LogoBase64 from './LogoBase64';
 
 const StyledImg = styled.img`
   width: 150px;
   display: block;
-  margin: 50px auto 0;
+  margin: 0 auto 0;
 `;
 
 const Logo = () => {
   return (
     <Link to="/">
-      <StyledImg src={LogoBase64} alt="ssuniverse-logo" />
+      <StyledImg src="/images/ssuniverse-logo.png" alt="ssuniverse-logo" />
     </Link>
   );
 };
