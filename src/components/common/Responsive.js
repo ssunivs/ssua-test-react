7def6ceb0d90c38105d1f4b2d44a7924046d7e78
clroot <abcdkh1209@gmail.com>
2021-11-04 14:13:58 +0900

    Responsive 컴포넌트 수정
    - Footer 영역 확보

--- src/components/common/Responsive.js
+++ src/components/common/Responsive.js
@@ -8,12 +8,13 @@ const ResponsiveBlock = styled.div`
   padding: 10px 1rem 25px;
   margin: 0 auto 0;
   text-align: center;
+  min-height: calc(100vh - 250px); // Footer 를 위한 영역
 
   @media (${bp.small}) {
     width: 95vw;
   }
   @media (${bp.medium}) {
-    max-width: 900px; //TODO: px 얼마나 잡을지
+    max-width: 900px;
   }
 `;
 
